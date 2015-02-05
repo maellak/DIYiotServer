8b824b2af6f2c345a9c6f371836370489198ed3a
prosf <grgs.vsl@gmail.com>
2015-02-05 08:24:40 -0500

    Fix splitted incoming sensor data by joining them

--- ws/mongopush.php
+++ ws/mongopush.php
@@ -3,18 +3,18 @@ require __DIR__ . '/vendor/autoload.php';
 
 $context = new ZMQContext();
 $socket = $context->getSocket(ZMQ::SOCKET_PUSH, 'my pusher');
-$socket->connect("tcp://127.0.0.1:5556");       
+$socket->connect("tcp://127.0.0.1:5556");
 // apo dw ksekinaei
 /*
 // gia test apo edo 
                 $socket->send("Continue\n");
-			$entryData = array(
-			    'catecory' => 'testdev',
-			    //'catecory' => $DEV,
-			    'a'   => "1",
-			    'when'    => time()
+                        $entryData = array(
+                            'catecory' => 'testdev',
+                            //'catecory' => $DEV,
+                            'a'   => "1",
+                            'when'    => time()
 );
-			$socket->send(json_encode($entryData));
+                        $socket->send(json_encode($entryData));
 // test 
 */
 /*
@@ -29,6 +29,7 @@ $socket1 = fsockopen("localhost",50036 );
 if(!$socket1)return;
 stream_set_blocking($socket1, 0);
 stream_set_blocking(STDIN, 0);
+$continue = false;
 do {
         $read   = array( $socket1, STDIN); $write  = NULL; $except = NULL;
 
@@ -44,20 +45,57 @@ do {
                 echo "\r";
                 $data = trim(fgets($socket1, 4096));
                 if($data != "") {
-	// edo ftiachete ta data pou erchonte apo to device
+// edo ftiachete ta data pou erchonte apo to device
+
+		/* ************************ */
+
+		$length = strlen($data);
+		$pos_a = strpos($data, "@");	
+		$pos_b = strpos($data, "#");
+			
+		if( ($pos_a === 0) && ($pos_b != false) ){
+			$data = substr($data, 1, $length-2);
+			$continue = true;
+			$data_a = "";
+			$data_b = "";
+		}
+		elseif($pos_a > 0){
+			$data_a = substr($data, $pos_a+1);
+			$data_b = "";
+			$continue = false;
+		}
+		elseif( ($pos_a === false) && ($pos_b > 0) ){
+			$data_b = substr($data, 0, $pos_b);
+			$data = $data_a . $data_b;
+			$continue = true;
+		}
+		elseif($pos_b < $length){
+			$data_b = substr($data, $pos_b+1);
+			$continue = false;
+		}
+		elseif( ($data_a != "") && ($data_b != "") ){
+			$data = $data_a . $data_b;
+			$continue = true;
+			$data_a = "";
+			$data_b = "";
+		}					
+									
+		if($continue === true) {
 			$entryData = array(
-			    'catecory' => 'ourdevice',
-			    'data'   => $data,
-			    'when'    => time()
+				'catecory' => 'ourdevice',
+				'data'   => $data,
+				'when'    => time()
 			);
 
-
-		  //edo ta stelneis gia na egrafoun
-   		  // choris kamia kathisterissi
-		  // ta archeia ta ipodechete to mongodb.php
-  		  // pou einai kai o pipe server gia tin mongo
-			$socket->send(json_encode($entryData));
-                }
-        }
+			//edo ta stelneis gia na egrafoun
+			// choris kamia kathisterissi
+			// ta archeia ta ipodechete to mongodb.php
+			// pou einai kai o pipe server gia tin mongo
+                        $socket->send(json_encode($entryData));
+		}
+	}	
+                 
+      }
 
 } while(true);
+
